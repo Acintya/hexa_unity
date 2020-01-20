8644def5ad695382163e95f4501c4e59e9b97059
ling <lingsheng.hs@gmail.com>
2020-01-20 18:48:23 +0100

    added NetMQ for communication with python server
    comm mode: REQ
    
    updated UI

---
+++ Assets/Skripts/HexaClient.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HexaClient : MonoBehaviour
+{
+    public List<InputField> IKPParamInput;
+    public List<InputField> IKPResOutput;
+
+    private HexaRequester _hexaRequester;
+    private List<float> IKPParams;
+    private List<float> IKPResults;
+    private List<float> FKPParams;
+
+    private void Start()
+    {
+        _hexaRequester = new HexaRequester();
+        _hexaRequester.Start();
+        IKPParams = new List<float>();
+        IKPResults = new List<float>();
+    }
+
+    private void OnDestroy()
+    {
+        _hexaRequester.Stop();
+    }
+
+    [ContextMenu("IKP")]
+    public void IKP()
+    {
+        Debug.Log("IKP");
+        UpdateIKPParameters();
+        IKPResults = _hexaRequester.ReqIKP(IKPParams);
+        UpdateIKPResults();
+    }
+
+    private void UpdateIKPParameters()
+    {
+        IKPParams.Clear();
+        for (int i = 0; i < IKPParamInput.Count; i++)
+        {
+            IKPParams.Add(float.Parse(IKPParamInput[i].text));
+        }
+    }
+
+    private void UpdateIKPResults()
+    {
+        for (int i = 0; i < IKPResults.Count; i++)
+        {
+            IKPResOutput[i].text = IKPResults[i].ToString();
+        }
+    }
+}
---
+++ Assets/Skripts/HexaRequester.cs
@@ -0,0 +1,83 @@
+﻿using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public struct IKPParameters
+{
+    public string Type;
+    public List<float> EndEffectorPosition;
+    public List<float> EndEffectorOrientation;
+}
+
+public struct FKPParameters
+{
+    public string Type;
+    public List<float> AcuatorParams;
+}
+
+public struct IKPResult
+{
+    public List<float> AcuatorAngels;
+}
+
+/// <summary>
+///     Example of requester who only sends Hello. Very nice guy.
+///     You can copy this class and modify Run() to suits your needs.
+///     To use this class, you just instantiate, call Start() when you want to start and Stop() when you want to stop.
+/// </summary>
+public class HexaRequester : RunAbleThread
+{
+    
+    /// <summary>
+    ///     Request Hello message to server and receive message back. Do it 10 times.
+    ///     Stop requesting when Running=false.
+    /// </summary>
+    protected override void Run()
+    {
+        ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
+        using (RequestSocket client = new RequestSocket())
+        {
+            client.Connect("tcp://localhost:5555");
+        }
+        NetMQConfig.Cleanup(); // this line is needed to prevent unity freeze after one use, not sure why yet
+    }
+
+    public List<float> ReqIKP(List<float> IKPParameters)
+    {
+        ForceDotNet.Force();
+        IKPResult result = new IKPResult();
+        using (RequestSocket client = new RequestSocket())
+        {
+            client.Connect("tcp://localhost:5555");
+
+            IKPParameters IKPParams = new IKPParameters();
+            IKPParams.Type = "IKP";
+            IKPParams.EndEffectorPosition = new List<float>();
+            IKPParams.EndEffectorOrientation = new List<float>();
+            for (int i = 0; i < 3; i++)
+            {
+                IKPParams.EndEffectorPosition.Add(IKPParameters[i]);
+                IKPParams.EndEffectorOrientation.Add(IKPParameters[i + 3]);
+            }
+            string reqIKP = JsonConvert.SerializeObject(IKPParams, Formatting.Indented);
+            client.SendFrame(reqIKP);
+
+            string message = null;
+            bool gotMessage = false;
+            while (Running)
+            {
+                gotMessage = client.TryReceiveFrameString(out message); // this returns true if it's successful
+                if (gotMessage) break;
+            }
+            result = JsonConvert.DeserializeObject<IKPResult>(message);
+            if (gotMessage) Debug.Log("Received " + message);
+
+        }
+        NetMQConfig.Cleanup();
+        return result.AcuatorAngels;
+    }
+}
